a53b6f6ceeb03c98e01b80516626cb3f3769bec7
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2024-11-08 15:08:52 +0000

    fixed: All import package and module errors and comments

--- week_0/matrix_multiplication/TestMatrixMultiplication.java
+++ week_0/matrix_multiplication/TestMatrixMultiplication.java
@@ -1,9 +1,13 @@
+package week_0.matrix_multiplication;
+
 public class TestMatrixMultiplication {
+
     public static void main(String[] args) {
         MatrixMultiplication mat = new MatrixMultiplication();
-        int [][]matA = mat.create2DMatrix();
-        int [][]matB = mat.create2DMatrix();
+        int[][] matA = mat.create2DMatrix();
+        int[][] matB = mat.create2DMatrix();
         mat.multiplyMatrix(matA, matB);
         mat.displayResult();
     }
+}
 
