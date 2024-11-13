496a42f66c4f13b1a25b93d8259cee549c90c8aa
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2024-11-13 14:21:33 +0000

    Created: new javafx project to save the previous configuration

---
+++ library_management_system/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.library_management_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.library_management_system to javafx.fxml;
+    exports com.example.library_management_system;
+}
