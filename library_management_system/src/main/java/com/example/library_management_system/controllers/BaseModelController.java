8288f4e10c08da83280ae08cc22a0e6dc207099a
nnyabe.amt <nnyabe.chamamme@amalitech.com>
2024-11-14 05:36:06 +0000

    Create BaseModelController.java

---
+++ library_management_system/src/main/java/com/example/library_management_system/controllers/BaseModelController.java
@@ -0,0 +1,97 @@
+package com.example.library.controllers;
+
+import com.example.library.exceptions.MySQLConnectionException;
+import com.example.library.utils.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseModelController<T> {
+
+    // Abstract method that subclasses should implement to convert a ResultSet row to a model object
+    protected abstract T mapRowToModel(ResultSet resultSet) throws SQLException;
+
+    // Get all records from the table
+    public List<T> getAll() throws SQLException {
+        List<T> results = new ArrayList<>();
+        String query = "SELECT * FROM " + getTableName();  // The table name should be provided by subclass
+
+        try (Connection connection = DBConnection.createConnection();
+             Statement statement = connection.createStatement();
+             ResultSet result = statement.executeQuery(query)) {
+
+            while (result.next()) {
+                results.add(mapRowToModel(result));
+            }
+        } catch (MySQLConnectionException e) {
+            throw new SQLException("Error fetching data: " + e.getMessage());
+        }
+
+        return results;
+    }
+
+    // Get record by ID
+    public T getById(int id) throws SQLException {
+        String query = "SELECT * FROM " + getTableName() + " WHERE id = ?";
+        T model = null;
+
+        try (Connection connection = DBConnection.createConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, id);
+            try (ResultSet result = preparedStatement.executeQuery()) {
+                if (result.next()) {
+                    model = mapRowToModel(result);
+                }
+            }
+        } catch (MySQLConnectionException e) {
+            throw new SQLException("Error fetching data by ID: " + e.getMessage());
+        }
+
+        return model;
+    }
+
+    // Delete record by ID
+    public boolean deleteById(int id) throws SQLException {
+        String query = "DELETE FROM " + getTableName() + " WHERE id = ?";
+        int rowsAffected = 0;
+
+        try (Connection connection = DBConnection.createConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, id);
+            rowsAffected = preparedStatement.executeUpdate();
+        } catch (MySQLConnectionException e) {
+            throw new SQLException("Error deleting record: " + e.getMessage());
+        }
+
+        return rowsAffected > 0;
+    }
+
+    // Update record by ID
+    public boolean updateById(T model) throws SQLException {
+        String query = getUpdateQuery();  // This should be implemented in the subclass
+        int rowsAffected = 0;
+
+        try (Connection connection = DBConnection.createConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            setUpdateParameters(preparedStatement, model);  // Set the parameters using the model
+            rowsAffected = preparedStatement.executeUpdate();
+        } catch (MySQLConnectionException e) {
+            throw new SQLException("Error updating record: " + e.getMessage());
+        }
+
+        return rowsAffected > 0;
+    }
+
+    // Method to get the table name (should be implemented in the subclass)
+    protected abstract String getTableName();
+
+    // Method to get the query for updating records (should be implemented in the subclass)
+    protected abstract String getUpdateQuery();
+
+    // Method to set update parameters for the PreparedStatement (should be implemented in the subclass)
+    protected abstract void setUpdateParameters(PreparedStatement preparedStatement, T model) throws SQLException;
+}
