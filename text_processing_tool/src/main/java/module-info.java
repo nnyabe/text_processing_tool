4e1a62258b4468dd66fbfc08751ae175871d70f1
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2024-11-22 08:25:29 +0000

    Created: the project init files

---
+++ text_processing_tool/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.text.text_processing_tool {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.text.text_processing_tool to javafx.fxml;
+    exports com.text.text_processing_tool;
+}
